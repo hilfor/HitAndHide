c214ea44af6a488bdd7daf025a0ed59a96566e3f
Daniel Gelbshtein <daniel.kantorov@gmail.com>
2015-11-15 08:10:47 +0200

    Some stuff

---
+++ Assets/Scripts/MultiplayerGameController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MultiplayerGameController : MonoBehaviour {
+
+	public GameObject standbyCamera;
+
+	public GameObject[] availableSpawnPoints;
+
+	private PlayerSpawnPoint[] spawnPoints;
+
+
+	void Start(){
+		spawnPoints = new PlayerSpawnPoint[availableSpawnPoints.Length];
+		for (int spawnPointIndex = 0 ; spawnPointIndex< availableSpawnPoints.Length; spawnPointIndex++){
+			spawnPoints[spawnPointIndex] = availableSpawnPoints[spawnPointIndex].GetComponent<PlayerSpawnPoint>();
+		}
+	}
+
+	void Update(){}
+		
+}
---
+++ Assets/Scripts/PlayerSpawnPoint.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSpawnPoint : MonoBehaviour {
+
+	public float minSpawnRadius;
+	public float maxSpawnRadius;
+
+	public int maxPlayersNearSpawn;
+	
+
+}
